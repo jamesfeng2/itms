aed90d949aa867d2b6071da2e0ed1e1e2b038394
jamesfeng2 <jamesfeng2@users.noreply.github.com>
2019-03-24 22:28:12 +1100

    Add files via upload

---
+++ src/assets/js/some-scripts.js
@@ -0,0 +1,17 @@
+$(document).ready( function(){
+$('#d-menu1 button').click(function(){
+  $('#dropdown1').text($(this).text());
+});
+
+$('#d-menu2 button').click(function(){
+  $('#dropdown2').text($(this).text());
+});
+
+$('#d-menu3 button').click(function(){
+  $('#dropdown3').text($(this).text());
+});
+
+$('#d-menu4 button').click(function(){
+  $('#dropdown4').text($(this).text());
+});
+});
